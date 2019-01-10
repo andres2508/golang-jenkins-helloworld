40e60e82237de36a3b1cdee89231894624f2a35c
Andres Aristizabal <jaime.aristizabal.2508@gmail.com>
2019-01-10 09:02:08 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# STEP 1 build executable binary
+FROM golang:alpine as builder
+# Install git
+RUN apk update && apk add git 
+#ENV GOPATH="~/go"
+COPY ./go-service $GOPATH/src/go-service
+# COPY prueba.txt /hello/prueba.txt
+WORKDIR $GOPATH/src/go-service
+#get dependancies
+#you can also use dep
+RUN go get -d -v
+#build the binary
+# RUN go build -o /go/bin/server
+
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags="-w -s" -o /go/bin/backoffice
+
+# STEP 2 build a small image
+# start from scratch 
+# ----- NOTA ------
+# Me toco cambiar scratch porque este no tiene manejador de paquetes para las credenciales de aws
+# FROM traefik:alpine
+FROM scratch
+# FROM ubuntu
+# Copy our static executable
+# RUN mkdir -p /go/bin
+COPY --from=builder /go/bin/backoffice /go/bin/backoffice
+# RUN export AWS_CONFIG_FILE = /root/.aws
+EXPOSE 3535
+ENTRYPOINT ["/go/bin/backoffice"]
+
+
+# WORKDIR $GOPATH/src/pekiz_tools_api
+# RUN ls -la
+# EXPOSE 5555
+# ENTRYPOINT ["go", "run", "server.go"]
---
+++ go-service/server.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+// -----------------------------------------------------
+// ----------------- Data Structures -------------------
+// -----------------------------------------------------
+
+type app struct {
+	Router *mux.Router
+}
+
+type ResponseUpload struct {
+	URL  string `json:"url_file"`
+	Hash string `json:"hash_name"`
+}
+
+// -----------------------------------------------------
+// ---------------- Server Conections ------------------
+// -----------------------------------------------------
+
+func (a *app) initializeRoutes() {
+	// AWS Tools API
+	a.Router.HandleFunc("/", a.helloworld).Methods("GET", "OPTIONS")
+}
+
+func (a *app) serve(addr string) {
+	handler := cors.AllowAll().Handler(a.Router)
+	http.ListenAndServe(addr, handler)
+}
+
+func (a *app) initialize() {
+
+	// Server Mux
+
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+	a.serve(":3535")
+}
+
+// -----------------------------------------------------
+// ---------------- Server Functions  ------------------
+// -----------------------------------------------------
+
+func (a *app) helloworld(w http.ResponseWriter, r *http.Request) {
+	log.Print("service helloworld consult by: " + r.RemoteAddr)
+
+	checkError(nil, w)
+	json.NewEncoder(w).Encode("Hello World 2!")
+}
+
+// -----------------------------------------------------
+// ----------------- Helper Functions ------------------
+// -----------------------------------------------------
+
+// Este metodo permite administrar los errores que puedan
+// surgir al momento de generar alguna consulta en el API
+// y envia la respuesta en el cuerpo del request
+func checkError(err error, w http.ResponseWriter) bool {
+	if err != nil {
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		s := err.Error()
+		w.Write([]byte(s))
+		return true
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		return false
+	}
+}
+
+// -----------------------------------------------------
+// ------------------ Main Structure -------------------
+// -----------------------------------------------------
+func main() {
+
+	// cntxt := &daemon.Context{
+	// 	PidFileName: "PID_FILE.pid",
+	// 	PidFilePerm: 0644,
+	// 	LogFileName: "LOG_APP.log",
+	// 	LogFilePerm: 0640,
+	// 	WorkDir:     "./",
+	// 	Umask:       027,
+	// 	Args:        []string{"[go-daemon sample]"},
+	// }
+
+	// d, err := cntxt.Reborn()
+	// if err != nil {
+	// 	log.Fatal("Unable to run: ", err)
+	// }
+	// if d != nil {
+	// 	return
+	// }
+	// defer cntxt.Release()
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("		  Daemon Started		")
+	log.Print("- - - - - - - - - - - - - - -")
+
+	// ---------- Start System ----------
+
+	a := app{}
+
+	a.initialize()
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("			Run System			")
+	log.Print("- - - - - - - - - - - - - - -")
+}
